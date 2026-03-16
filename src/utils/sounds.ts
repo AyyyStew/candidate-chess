f9d71e864eb1c9786e2e171b4f14d3653b00675b
AyyyStew <alexsteward98@gmail.com>
2026-03-15 23:41:39 -0500

    added jingle for game completion

--- src/utils/sounds.ts
+++ src/utils/sounds.ts
@@ -1,9 +1,14 @@
 const PIECE_SOUNDS = Object.values(
-  import.meta.glob("../assets/sounds/edited/pieces/*", { eager: true, as: "url" }),
+  import.meta.glob("../assets/sounds/edited/pieces/*", {
+    eager: true,
+    as: "url",
+  }),
 ).map((src) => new Audio(src as string));
 
 import pieceBounceUrl from "../assets/sounds/edited/piece_bounce.ogg";
+import successJingleUrl from "../assets/sounds/edited/successJingle.wav";
 const BOUNCE_SOUND = new Audio(pieceBounceUrl);
+const SUCCESS_JINGLE = new Audio(successJingleUrl);
 
 export function playMoveSound() {
   const sound = PIECE_SOUNDS[Math.floor(Math.random() * PIECE_SOUNDS.length)];
@@ -14,4 +19,9 @@ export function playMoveSound() {
 export function playBounceSound() {
   BOUNCE_SOUND.currentTime = 0;
   BOUNCE_SOUND.play().catch(() => {});
+}
+
+export function playSuccessJingle() {
+  SUCCESS_JINGLE.currentTime = 0;
+  SUCCESS_JINGLE.play().catch(() => {});
 }
